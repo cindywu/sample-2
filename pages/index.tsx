42543bb6169ba77deae09c133cb84b357924be85
cindy <1177031+cindywu@users.noreply.github.com>
2023-05-13 18:53:39 -1000

    checkpoint 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import Hello from '../components/hello'
-export default function Home() {
+import { supabase } from '../lib/supabaseClient'
+
+export default function Home({ countries }) {
   return (
     <div className={'bg-green-200'}>
       hello world
       <Hello />
+      <ul>
+        {countries.map((country) => (
+          <li key={country.id}>{country.name}</li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  let { data } = await supabase.from('countries').select()
+  return {
+    props: {
+      countries: data,
+    },
+  }
 }
